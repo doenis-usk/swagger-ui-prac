4dd72ecda645a497e62a0e314c24c6c51ba5a370
yusuke <doenis05@gmail.com>
2021-12-24 23:47:20 +0900

    initial commit

---
+++ src/main/java/com/example/swaggeruiprac/SwaggerUiPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggeruiprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUiPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUiPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swaggeruiprac/SwaggerUiPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggeruiprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerUiPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
